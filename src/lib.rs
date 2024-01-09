959c522777f030d70e36fef5625239145c3dbdf3
mateuszkaczmarczyk <mateusz.kaczmarczyk@tylko.com>
2024-01-09 23:16:04 +0100

    setup wgpu

--- src/lib.rs
+++ src/lib.rs
@@ -3,28 +3,177 @@ use winit::{
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
+use winit::window::Window;
+use winit::event::WindowEvent;
 
-pub fn run() {
+pub struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+    window: Window,
+}
+
+impl State {
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+     async fn new(window: Window) -> Self {
+         let size = window.inner_size();
+         let instance_options = wgpu::InstanceDescriptor {
+             backends: wgpu::Backends::all(),
+             ..Default::default()
+         };
+         let instance = wgpu::Instance::new(instance_options);
+         let surface = unsafe { instance
+             .create_surface(&window)
+             .unwrap()
+         };
+         let adapter_options = &wgpu::RequestAdapterOptions {
+             power_preference: wgpu::PowerPreference::HighPerformance,
+             compatible_surface: Some(&surface),
+             force_fallback_adapter: false
+         };
+         let adapter = instance
+             .request_adapter(adapter_options)
+             .await.unwrap();
+         let device_options = &wgpu::DeviceDescriptor {
+             features: wgpu::Features::empty(),
+             limits: wgpu::Limits::default(),
+             label: None,
+         };
+         let (device, queue) = adapter
+             .request_device(device_options, None)
+             .await.unwrap();
+         let surface_capabilities = surface.get_capabilities(&adapter);
+         let surface_format = surface_capabilities.formats
+             .iter()
+             .copied()
+             .filter(|f| f.is_srgb())
+             .next()
+             .unwrap_or(surface_capabilities.formats[0]);
+         let config = wgpu::SurfaceConfiguration {
+             usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+             format: surface_format,
+             width: size.width,
+             height: size.height,
+             present_mode: surface_capabilities.present_modes[0],
+             alpha_mode: surface_capabilities.alpha_modes[0],
+             view_formats: vec![],
+         };
+         surface.configure(&device, &config);
+
+         Self {
+             window,
+             surface,
+             device,
+             queue,
+             config,
+             size,
+         }
+     }
+
+    fn input(&mut self, _event: &WindowEvent) -> bool {
+        false
+    }
+
+    fn update(&mut self) {}
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+        let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+        let encoder_options = &wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        };
+        let mut encoder = self.device.create_command_encoder(encoder_options);
+
+        {
+            let bg_color = wgpu::Color {
+                r: 0.1,
+                g: 0.2,
+                b: 0.3,
+                a: 1.0,
+            };
+            let pass_color_attachment = wgpu::RenderPassColorAttachment {
+                view: &view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(bg_color),
+                    store: wgpu::StoreOp::Store,
+                },
+            };
+            let pass_options = &wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(pass_color_attachment)],
+                depth_stencil_attachment: None,
+                occlusion_query_set: None,
+                timestamp_writes: None,
+            };
+
+            let _render_pass = encoder.begin_render_pass(pass_options);
+        }
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.height > 0 && new_size.width > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+ }
+
+pub async fn run() {
     env_logger::init();
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
-    event_loop.run(move |event, _, control_flow| match event {
+    let mut state = State::new(window).await;
+
+    event_loop.run(move |event, _, control_flow|
+        match event {
+        Event::RedrawRequested(window_id) if window_id == state.window().id() => {
+            state.update();
+            match state.render() {
+                Ok(_) => {}
+                Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                Err(e) => eprintln!("{:?}", e),
+            }
+        }
+        Event::MainEventsCleared => {
+            state.window().request_redraw();
+        }
         Event::WindowEvent {
-            ref event,
-            window_id,
-        } if window_id == window.id() => match event {
-            WindowEvent::CloseRequested
-            | WindowEvent::KeyboardInput {
-                input:
-                KeyboardInput {
-                    state: ElementState::Pressed,
-                    virtual_keycode: Some(VirtualKeyCode::Escape),
+                ref event,
+                window_id,
+        } if window_id == state.window().id() => if !state.input(event) {
+            match event {
+                WindowEvent::CloseRequested | WindowEvent::KeyboardInput {
+                    input:
+                    KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                        ..
+                    },
                     ..
-                },
-                ..
-            } => *control_flow = ControlFlow::Exit,
-            _ => {}
+                } => *control_flow = ControlFlow::Exit,
+                WindowEvent::Resized(physical_size) => {
+                    state.resize(*physical_size);
+                }
+                WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                    state.resize(**new_inner_size);
+                }
+                _ => {}
+            }
         },
         _ => {}
     });
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use cgfs_with_wgpu::run;
 
 fn main() {
-    run();
+    pollster::block_on(run());
 }
