a44d00921e154420e85c4581da9e94bed1c1da92
mateuszkaczmarczyk <mateusz.kaczmarczyk@tylko.com>
2024-01-17 00:16:04 +0100

    setup simple projection

--- src/rasterizer.rs
+++ src/rasterizer.rs
@@ -7,8 +7,28 @@ pub struct Rasterizer {
 
 impl Rasterizer {
     pub const CANVAS: [i32; 2] = [ 1600, 1600 ];
+    // Canvas size
+
+    pub const VIEWPORT: [f32; 3] = [1.0, 1.0, 1.0];
+    // Viewport width, height and depth which is camera distance
+
+    pub const CAMERA_POSITION: [f32; 3] = [0.0, 0.0, 0.0];
+
     pub fn new() -> Self {
         Self { state: vec![] }
+    }
+
+    pub fn project_vertex(&mut self, vertex: [f32; 3]) -> [i32; 2] {
+        fn viewport_to_canvas(x: f32, y: f32) -> [i32; 2] {
+            let x_pos = x * Rasterizer::CANVAS[0] as f32 / Rasterizer::VIEWPORT[0] as f32;
+            let y_pos = y * Rasterizer::CANVAS[1] as f32 / Rasterizer::VIEWPORT[1] as f32;
+            return [x_pos as i32, y_pos as i32];
+        }
+
+        let x_projection = vertex[0] * Rasterizer::VIEWPORT[2] / vertex[2];
+        let y_projection = vertex[1] * Rasterizer::VIEWPORT[2] / vertex[2];
+
+        return viewport_to_canvas(x_projection, y_projection);
     }
 
     pub fn get_state(&mut self) -> &[Vertex] {
@@ -110,12 +130,56 @@ impl Rasterizer {
 pub fn init_rasterizer() -> Rasterizer {
     let mut rasterizer = Rasterizer::new();
 
-    let point_a = [-700, 600];
-    let point_b = [540, 120];
-    let point_c = [-50, -700];
+    // let point_a = [-700, 600];
+    // let point_b = [540, 120];
+    // let point_c = [-50, -700];
+    //
+    // rasterizer.draw_wireframe_triangle(point_a, point_b, point_c, [0.0, 0.0, 0.0]);
+    // rasterizer.draw_filled_triangle(point_a, point_b, point_c, [0.0, 255.0, 0.0]);
 
-    rasterizer.draw_wireframe_triangle(point_a, point_b, point_c, [0.0, 0.0, 0.0]);
-    rasterizer.draw_filled_triangle(point_a, point_b, point_c, [0.0, 255.0, 0.0]);
+    // The four "front" vertices
+    let vAf = [-2.0, -0.5, 5.0];
+    let vBf = [-2.0,  0.5, 5.0];
+    let vCf = [-1.0,  0.5, 5.0];
+    let vDf = [-1.0, -0.5, 5.0];
+
+    // The four "back" vertices
+    let vAb = [-2.0, -0.5, 6.0];
+    let vBb = [-2.0,  0.5, 6.0];
+    let vCb = [-1.0,  0.5, 6.0];
+    let vDb = [-1.0, -0.5, 6.0];
+
+    const PURPLE: [f32; 3] = [255.0, 0.0, 255.0];
+    const RED: [f32; 3] = [255.0, 0.0, 0.0];
+    const GREEN: [f32; 3] = [0.0, 255.0, 0.0];
+
+    let a = rasterizer.project_vertex(vAf);
+    let b = rasterizer.project_vertex(vBf);
+    let c = rasterizer.project_vertex(vCf);
+    let d = rasterizer.project_vertex(vDf);
+
+    let e = rasterizer.project_vertex(vAb);
+    let f = rasterizer.project_vertex(vBb);
+    let g = rasterizer.project_vertex(vCb);
+    let h = rasterizer.project_vertex(vDb);
+
+    // The front face
+    rasterizer.draw_line(a, b, PURPLE);
+    rasterizer.draw_line(b, c, PURPLE);
+    rasterizer.draw_line(c, d, PURPLE);
+    rasterizer.draw_line(d, a, PURPLE);
+
+    // The back face
+    rasterizer.draw_line(e, f, RED);
+    rasterizer.draw_line(f, g, RED);
+    rasterizer.draw_line(g, h, RED);
+    rasterizer.draw_line(h, e, RED);
+
+    // The front-to-back edges
+    rasterizer.draw_line(a, e, GREEN);
+    rasterizer.draw_line(b, f, GREEN);
+    rasterizer.draw_line(c, g, GREEN);
+    rasterizer.draw_line(d, h, GREEN);
 
     return rasterizer;
 }
